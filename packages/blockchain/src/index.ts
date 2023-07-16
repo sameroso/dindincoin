792f239194569745eaa8bbb2308a306f4e188bb1
Samer Kayali <samerkkr@gmail.com>
2023-07-16 15:43:57 -0300

    feat(Blockchain): added server in port 3035

--- packages/blockchain/src/index.ts
+++ packages/blockchain/src/index.ts
@@ -1,5 +1,15 @@
 import { Blockchain } from "./Blockchain";
+import http from "http";
 
 const blockchain = new Blockchain();
 
-console.log(blockchain.hash({ test1: "Test1", test: "test2" }));
+http
+  .createServer((req, res) => {
+    res.end(
+      `<h1>Dindincoin Blockchain</h1>
+      <h2>${JSON.stringify(blockchain.previousBlock)}</h2>`
+    );
+  })
+  .listen(3035, () => {
+    console.log("listening on port 3035");
+  });
